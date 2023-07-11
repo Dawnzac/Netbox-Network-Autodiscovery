c0f0af0c74386b08ee41077d58906ff7bd78bd8a
Dawn Zac <43729160+Dawnzac@users.noreply.github.com>
2023-07-11 15:01:24 +0530

    Update nmap.py

--- nmap.py
+++ nmap.py
@@ -8,46 +8,51 @@ hosts = list()
 unknown = "not found"
 
 def run():
-    tree = ET.parse("10.80.30.0.xml")
-    root = tree.getroot()
-
-    for host in root.findall('host'):
-        #get mac address and vendor 
-        for address in host.iter('address'):
-             logging.debug('for loop inside address')
-             logging.debug({address.get('vendor')})
-             mac = address.get('addr')
-             ven = address.get('vendor')
-        try:
-            hosts.append((
-                host.find('address').attrib['addr'],
-                host.find('hostnames').find('hostname').attrib['name'],mac,ven,
-                host.find('os').find('osmatch').find('osclass').attrib['osfamily'],
-                host.find('os').find('osmatch').attrib['name']))
-        except AttributeError:
+    for f in os.listdir(self.path):
+        if not f.endswith('.xml'):
+            continue
+        abspath = os.path.join(self.path, f)
+        tree = ET.parse(abspath)
+        #tree = ET.parse("10.80.30.0.xml")
+        #root = tree.getroot()
+            
+        for host in root.findall('host'):
+            #get mac address and vendor 
+            for address in host.iter('address'):
+                logging.debug('for loop inside address')
+                logging.debug({address.get('vendor')})
+                mac = address.get('addr')
+                ven = address.get('vendor')
             try:
                 hosts.append((
                     host.find('address').attrib['addr'],
-                    ven,mac,ven,
+                    host.find('hostnames').find('hostname').attrib['name'],mac,ven,
                     host.find('os').find('osmatch').find('osclass').attrib['osfamily'],
-                    host.find('os').find('osmatch').attrib['name']
-                    ))
+                    host.find('os').find('osmatch').attrib['name']))
             except AttributeError:
                 try:
                     hosts.append((
                         host.find('address').attrib['addr'],
-                        host.find('hostnames').find('hostname').attrib['name'],
-                        mac,ven,
-                        unknown,
-                        unknown
+                        ven,mac,ven,
+                        host.find('os').find('osmatch').find('osclass').attrib['osfamily'],
+                        host.find('os').find('osmatch').attrib['name']
                         ))
                 except AttributeError:
-                    hosts.append((
-                        host.find('address').attrib['addr'],
-                        ven,mac,ven,
-                        unknown,
-                        unknown
-                        ))
+                    try:
+                        hosts.append((
+                            host.find('address').attrib['addr'],
+                            host.find('hostnames').find('hostname').attrib['name'],
+                            mac,ven,
+                            unknown,
+                            unknown
+                            ))
+                    except AttributeError:
+                        hosts.append((
+                            host.find('address').attrib['addr'],
+                            ven,mac,ven,
+                            unknown,
+                            unknown
+                            ))
         #logging.info(f'Address : {host.find("address").attrib["addr"]}')
         #logging.info(f'dns name : {host.find("hostnames").find("hostname").attrib["name"]}')
         #logging.info(f'description : {host.find("os").find("osmatch").find("osclass").attrib["osfamily"]}')
