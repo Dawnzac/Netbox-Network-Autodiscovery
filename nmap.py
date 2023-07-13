bea1c37e92a1b48893e09649231bb33023296ff2
Dawn Zac <43729160+Dawnzac@users.noreply.github.com>
2023-07-13 22:52:51 +0530

    Update nmap.py

--- nmap.py
+++ nmap.py
@@ -1,64 +1,63 @@
 import os
 import xml.etree.ElementTree as ET
 import logging
-import numpy as np
 
-path = os.getcwd()
-hosts = list()
-unknown = "not found"
+class Nmap(object):
 
-def run():
-    for f in os.listdir(path):
-            if not f.endswith('.xml'):
-                continue
-            abspath = os.path.join(path, f)
-            print(abspath)
-            tree = ET.parse(abspath)
-            root = tree.getroot()
-            for host in root.findall('host'):
-                for address in host.iter('address'):
-                    logging.debug('for loop inside address')
-                    logging.debug({address.get('vendor')})
-                    mac = address.get('addr')
-                    ven = address.get('vendor')
-                    #hosts.append((ipadr,mac,ven))
-                try:
-                    hosts.append((
-                        host.find('address').attrib['addr'],
-                        host.find('hostnames').find('hostname').attrib['name'],mac,ven,
-                        host.find('os').find('osmatch').find('osclass').attrib['osfamily'],
-                        host.find('os').find('osmatch').attrib['name']))
-                except AttributeError:
+    def __init__(self, path, unknown):
+        self.unknown = unknown
+        self.path = path
+        self.hosts = list()
+
+    def run(self):
+        for f in os.listdir(self.path):
+                if not f.endswith('.xml'):
+                    continue
+                abspath = os.path.join(self.path, f)
+                tree = ET.parse(abspath)
+                root = tree.getroot()
+                
+                for host in root.findall('host'):
+                    for address in host.iter('address'):
+                        logging.debug('for loop inside address')
+                        logging.debug({address.get('vendor')})
+                        mac = address.get('addr')
+                        ven = address.get('vendor')
+                        if ven is None:
+                        	ven = "Not-Found"
+                        fven= ven.replace(' ','-').replace('(','').replace(')','').replace(',','-').replace('.','-')
+                        
                     try:
-                        hosts.append((
+                        self.hosts.append((
                             host.find('address').attrib['addr'],
-                            ven,mac,ven,
+                            host.find('hostnames').find('hostname').attrib['name'],mac,ven,
                             host.find('os').find('osmatch').find('osclass').attrib['osfamily'],
-                            host.find('os').find('osmatch').attrib['name']
-                            ))
+                            host.find('os').find('osmatch').attrib['name']))
                     except AttributeError:
                         try:
-                            hosts.append((
+                            self.hosts.append((
                                 host.find('address').attrib['addr'],
-                                host.find('hostnames').find('hostname').attrib['name'],
-                                mac,ven,
-                                unknown,
-                                unknown
+                                fven,mac,ven,
+                                host.find('os').find('osmatch').find('osclass').attrib['osfamily'],
+                                host.find('os').find('osmatch').attrib['name']
                                 ))
                         except AttributeError:
-                            hosts.append((
-                                host.find('address').attrib['addr'],
-                                ven,mac,ven,
-                                unknown,
-                                unknown
-                                ))
-                #logging.info(f'Address : {host.find("address").attrib["addr"]}')
-                #logging.info(f'dns name : {host.find("hostnames").find("hostname").attrib["name"]}')
-                #logging.info(f'description : {host.find("os").find("osmatch").find("osclass").attrib["osfamily"]}')
-                #logging.info(f'comments : {host.find("os").find("osmatch").attrib["name"]}')
-        
-if __name__== "__main__":
-	run();
-	#uncomment below line to see results in cli
-	#print(*hosts, sep = "\n")
-	np.savetxt("uploadfile.csv", hosts, delimiter=", ", fmt ='% s')
+                            try:
+                                self.hosts.append((
+                                    host.find('address').attrib['addr'],
+                                    host.find('hostnames').find('hostname').attrib['name'],
+                                    mac,ven,
+                                    self.unknown,
+                                    self.unknown
+                                    ))
+                            except AttributeError:
+                                self.hosts.append((
+                                    host.find('address').attrib['addr'],
+                                    fven,mac,ven,
+                                    self.unknown,
+                                    self.unknown
+                                    ))
+                    #logging.info(f'Address : {host.find("address").attrib["addr"]}')
+                    #logging.info(f'dns name : {host.find("hostnames").find("hostname").attrib["name"]}')
+                    #logging.info(f'description : {host.find("os").find("osmatch").find("osclass").attrib["osfamily"]}')
+                    #logging.info(f'comments : {host.find("os").find("osmatch").attrib["name"]}'
