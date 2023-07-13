ea2ef76cc4a108a1b96f3af08ebbf091c3f9e69b
Dawn Zac <43729160+Dawnzac@users.noreply.github.com>
2023-07-13 15:30:16 +0530

    Update nmap-scan.bat

--- scan automate/nmap-scan.bat
+++ scan automate/nmap-scan.bat
@@ -1,21 +1,8 @@
 @ECHO OFF
 CHCP 65001 > NUL
 
-
-set /A TODAY="$(date+%d.%m.%yT%H:%M:%S%Z)"
-echo %TODAY%
-
 FOR /F "usebackq delims="  %%I IN ("networks.txt") DO (
-    timeout 3
+    timeout 1
     echo %%I
-    scp D:/netbox/nmap/%%I nbx@10.80.80.80:/home/nbx/Downloads/netbox-scanner/
+    nmap "%%I" -T4 -O --system-dns --host-timeout 30s -oX nmap-"%%I".xml
 )
-::for net in "${NETWORKS[@]}"; do
-::    NETNAME=$(echo $net | tr -s '/' '-')
-::    echo $net
-    ::nmap "$net" -T4 -O -F --host-timeout 30s -oX nmap-"$NETNAME".xml
-    ::nmap "$net" -sU --script nbstat.nse -p137 -oX nmap-"$NETNAME".xml
-    ::nmap "$net" -T4 -O --system-dns --host-timeout 30s -oX nmap-"$NETNAME".xml
-    ::nmap "$net" -T4 -sn --host-timeout 30s -oX nmap-"$NETNAME".xml
-::done
-
